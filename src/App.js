a98499dc716c4a5253e0b2c3834428e3966bb82c
lefmarna <panume5ya@gmail.com>
2021-12-30 12:07:42 +0900

    learn prop-tyypes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 const App = () => {
   const profiles = [
     {
@@ -10,6 +12,7 @@ const App = () => {
     },
     {
       name: "NoName",
+      age: 3,
     },
   ];
   return (
@@ -29,8 +32,9 @@ const User = (props) => {
   );
 };
 
-User.defaultProps = {
-  age: 1,
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired,
 };
 
 export default App;
