c6c365e466a92536becdbfc633e96c5400e7c124
lefmarna <panume5ya@gmail.com>
2022-01-01 11:37:03 +0900

    inplement event new component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./index.css";
 import reducer from "./reducers";
 import EventsIndex from "./components/events_index";
+import EventsNew from "./components/events_new";
 import reportWebVitals from "./reportWebVitals";
 
 const store = createStore(reducer, applyMiddleware(thunk));
@@ -14,7 +16,12 @@ const store = createStore(reducer, applyMiddleware(thunk));
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <EventsIndex />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/events/new" element={<EventsNew />} />
+          <Route exact path="/" element={<EventsIndex />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
